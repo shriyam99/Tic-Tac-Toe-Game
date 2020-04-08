facdcdb76b33f940da204020c17fcc95b99868d8
Shriyam Tripathi <tripathishriyam99@gmail.com>
2020-04-09 00:50:14 +0530

    found minor bug in winner.js

--- server/utils/winner.js
+++ server/utils/winner.js
@@ -9,6 +9,12 @@ function isWinner(data){
     return true;
   else if(data[2] && data[4] && data[6])
     return true;
+  else if(data[0] && data[3] && data[6])
+    return true;
+  else if(data[1] && data[4] && data[7])
+    return true;
+  else if(data[2] && data[5] && data[8])
+    return true;
   else {
     return false;
   }
