def631afe275fcff38a2131b20bba1bfc9e4c417
Shriyam Tripathi <tripathishriyam99@gmail.com>
2020-04-09 13:31:07 +0530

    minor changes

--- public/js/game.js
+++ public/js/game.js
@@ -4,6 +4,20 @@ var sampleFunc = ()=>{
   document.getElementsByClassName('mainGame')[0].style.display = 'none';
 }
 const socket = io();
+function searchToObject() {
+  var searchString = window.location.search;
+  searchString = searchString.replace(/[+]/g, ' ');
+  var pairs = searchString.substring(1).split("&"),
+    obj = {},
+    pair,
+    i;
+  for ( i in pairs ) {
+    if ( pairs[i] === "" ) continue;
+    pair = pairs[i].split("=");
+    obj[ decodeURIComponent( pair[0] ) ] = decodeURIComponent( pair[1] );
+  }
+  return obj;
+}
 
 socket.on('connect', ()=>{
   console.log('User connected');
